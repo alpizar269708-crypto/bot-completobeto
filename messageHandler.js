6ac893e5ffc0d12a05d805d4cac19ea12521ef03
Beto <alpizar269708@gmail.com>
2026-09-21 21:28:20 -0600

    Eliminar comando salvar

--- messageHandler.js
+++ messageHandler.js
@@ -22,7 +22,7 @@ const { comandoRifa, comandoRifaInscripcion, comandoRifaJasc13, comandoMenuRifaJ
 const { comandoCarry } = require('./comandos/carry');
 
 const categoriasMap = {
-    'fortnite': ['pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'setpavos', 'setlegendarias', 'resetpavos', 'alertasstw', 'salvar', 'stw', 'alerta', 'setgrupostw', 'unsetgrupostw', 'setprecio'],
+    'fortnite': ['pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'setpavos', 'setlegendarias', 'resetpavos', 'alertasstw', 'stw', 'alerta', 'setgrupostw', 'unsetgrupostw', 'setprecio'],
     'economia': ['cartera', 'bal', 'banco', 'pay', 'pagar', 'top', 'topdinero', 'daily', 'weekly', 'farmear', 'work', 'crime', 'mendigar', 'pescar', 'minar', 'cazar', 'explorar', 'ruleta', 'cf', 'slots', 'dados', 'adivina', 'buscaminas', 'rob', 'ppt', 'pelea', 'carrera', 'hackear', 'shop', 'buy', 'inventario', 'mochila', 'vender', 'use', 'regalar'],
     'utilidades': ['s', 'sticker', 'todos', 'tiktok', 'traduce', 'skin', 'stats', 'contacto'],
     'ia': ['ia'],
@@ -97,7 +97,7 @@ Apoya a un creador: JASC13` });
     }
 
     const comandosValidos = [
-        'activarcomandos', 'setprecio', 'ping', 'pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'setpavos', 'setlegendarias', 'resetpavos', 'alertasstw', 'salvar', 'stw', 'alerta', 
+        'activarcomandos', 'setprecio', 'ping', 'pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'setpavos', 'setlegendarias', 'resetpavos', 'alertasstw', 'stw', 'alerta', 
         'setgrupostw', 'unsetgrupostw', 'grupo', 'mute', 'unmute', 'inactivos', 'tienda', 'ia', 'menu', 'menusecreto',
         's', 'sticker', 'todos', 'tiktok', 'traduce', 'skin', 'stats', 'contacto',
         'warn', 'advertir', 'verwarns', 'ban', 'unban', 'listanegra', 'banlist', 'unbanlist', 
