010b09f5ab335b2a2026ba41376dae7369845e0f
Beto <alpizar269708@gmail.com>
2026-09-22 11:09:09 -0600

    Cambiar comando plaltas a destacadasstw

--- messageHandler.js
+++ messageHandler.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 const { ejecutarMenu } = require('./comandos/menu');
-const { alertasSTW, comandoPLaltas, comandoPreguntarAlerta, activarAlertasDiarias, desactivarAlertasDiarias } = require('./comandos/fortnite');
+const { alertasSTW, comandoDestacadasSTW, comandoPreguntarAlerta, activarAlertasDiarias, desactivarAlertasDiarias } = require('./comandos/fortnite');
 const { comandoTiendaMenu, comandoTiendaCategoria } = require('./comandos/tienda'); 
 const { 
     comandoSticker, comandoTodos, comandoTiktok, comandoTraduce, comandoSkin, comandoStats, comandoContacto 
@@ -22,7 +22,7 @@ const { comandoRifa, comandoRifaInscripcion, comandoRifaJasc13, comandoMenuRifaJ
 const { comandoCarry } = require('./comandos/carry');
 
 const categoriasMap = {
-    'fortnite': ['pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'alertasstw', 'stw', 'alerta', 'setgrupostw', 'unsetgrupostw', 'setprecio'],
+    'fortnite': ['pavos', 'destacadasstw', 'legendariasstw', 'epicasstw', 'alertasstw', 'stw', 'alerta', 'setgrupostw', 'unsetgrupostw', 'setprecio'],
     'economia': ['cartera', 'bal', 'banco', 'pay', 'pagar', 'top', 'topdinero', 'daily', 'weekly', 'farmear', 'work', 'crime', 'mendigar', 'pescar', 'minar', 'cazar', 'explorar', 'ruleta', 'cf', 'slots', 'dados', 'adivina', 'buscaminas', 'rob', 'ppt', 'pelea', 'carrera', 'hackear', 'shop', 'buy', 'inventario', 'mochila', 'vender', 'use', 'regalar'],
     'utilidades': ['s', 'sticker', 'todos', 'tiktok', 'traduce', 'skin', 'stats', 'contacto'],
     'ia': ['ia'],
@@ -97,7 +97,7 @@ Apoya a un creador: JASC13` });
     }
 
     const comandosValidos = [
-        'activarcomandos', 'setprecio', 'ping', 'pavos', 'plaltas', 'legendariasstw', 'epicasstw', 'alertasstw', 'stw', 'alerta', 
+        'activarcomandos', 'setprecio', 'ping', 'pavos', 'destacadasstw', 'legendariasstw', 'epicasstw', 'alertasstw', 'stw', 'alerta', 
         'setgrupostw', 'unsetgrupostw', 'grupo', 'mute', 'unmute', 'inactivos', 'tienda', 'ia', 'menu', 'menusecreto',
         's', 'sticker', 'todos', 'tiktok', 'traduce', 'skin', 'stats', 'contacto',
         'warn', 'advertir', 'verwarns', 'ban', 'unban', 'listanegra', 'banlist', 'unbanlist', 
@@ -157,8 +157,8 @@ Apoya a un creador: JASC13` });
             case 'pavos':
                 await alertasSTW(sock, chatJid, msg, 'pavos');
                 break;
-            case 'plaltas':
-                await comandoPLaltas(sock, chatJid, msg);
+            case 'destacadasstw':
+                await comandoDestacadasSTW(sock, chatJid, msg);
                 break;
             case 'legendariasstw':
                 await alertasSTW(sock, chatJid, msg, 'legendarias');
